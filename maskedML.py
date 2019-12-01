f2ff3d48a42ce71dc80347f46fb5031808e2dc8d
mennaML <mennaML>
2019-12-01 14:02:20 -0500

    Loss and prediction scores

--- maskedML.py
+++ maskedML.py
@@ -1,31 +1,49 @@
 import torch
 from transformers import BertTokenizer, BertModel, BertForMaskedLM
+from random import randrange
 
 # OPTIONAL: if you want to have more information on what's happening under the hood, activate the logger as follows
 import logging
 logging.basicConfig(level=logging.INFO)
 
+
+
 # Load pre-trained model tokenizer (vocabulary)
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 # Tokenize input
-#text = '[CLS] I [MASK] to buy the car because it is cheap . [SEP]'
-text = "[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]"
+
+text = '[CLS] I want to buy the car because it is cheap . I like to run everyday. Tables are round. [SEP]'
 tokenized_text = tokenizer.tokenize(text)
 
-# Mask a token that we will try to predict back with `BertForMaskedLM`
-masked_index = 8
-tokenized_text[masked_index] = '[MASK]'
-assert tokenized_text == ['[CLS]', 'who', 'was', 'jim', 'henson', '?', '[SEP]', 'jim', '[MASK]', 'was', 'a', 'puppet', '##eer', '[SEP]']
-print(tokenized_text)
 
+#Determinig number of words to mask from percentage
+percentage = 20
+words_to_mask = int(percentage*0.01*len(tokenized_text))
+
+# Mask a token that we will try to predict back with `BertForMaskedLM`
+#masked_index = 10
+
+#Masking chosen percentage of tokens from text
+# masked = []
+masked = {}
+while words_to_mask>0:
+    masked_index= randrange(len(tokenized_text))
+    if masked_index not in masked:
+        masked[masked_index] = tokenized_text[masked_index]
+        tokenized_text[masked_index] = '[MASK]'
+        # masked.append(masked_index)
+        words_to_mask += -1
+
+print(tokenized_text)
+# assert tokenized_text == ['[CLS]', 'who', 'was', 'jim', 'henson', '?', '[SEP]', 'jim', '[MASK]', 'was', 'a', 'puppet', '##eer', '[SEP]']
 
 # Convert token to vocabulary indices
 indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
 # Define sentence A and B indices associated to 1st and 2nd sentences (see paper)
 
 segments_ids = [0] * len(tokenized_text)
-segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+# segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
 
 # Convert inputs to PyTorch tensors
 tokens_tensor = torch.tensor([indexed_tokens])
@@ -56,7 +74,7 @@ segments_tensors = torch.tensor([segments_ids])
 #     encoded_layers = outputs[0]
 # # We have encoded our input sequence in a FloatTensor of shape (batch size, sequence length, model hidden dimension)
 # assert tuple(encoded_layers.shape) == (1, len(indexed_tokens), model.config.hidden_size)
-# # -----------------------
+# # # -----------------------
 
 
 # Load pre-trained model (weights)
@@ -73,8 +91,10 @@ if use_cuda:
 # Predict all tokens
 
 #tokens_tensor = torch.tensor(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
-masks =  torch.tensor([  -1,  -1,  -1,  -1,  -1,  -1, -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1])
+masks =  torch.tensor(-1*torch.ones((tokens_tensor.shape[1]))).long()
 masks[masked_index] = 0
+
+print(masks)
 
 with torch.no_grad():
     outputs = model(tokens_tensor, masked_lm_labels=masks)
@@ -84,11 +104,16 @@ with torch.no_grad():
 #tokens_tensor = torch.tensor(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
 
 print('loss: ', loss.item() )
-#print('prediction_scores: ', prediction_scores)
+print('prediction_scores: ', prediction_scores.shape)
 
+words = {}
 
-predicted_index = torch.argmax(predictions[0, masked_index]).item()
-predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
-#assert predicted_token == 'henson'
+for index in masked:
+    predicted_index = torch.argmax(predictions[0,index]).item()
+    predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
+    # words.append(predicted_token)
+    words[masked[index]] = predicted_token
 
-print('predicted_token: ', predicted_token)
+print(words)
+
+#print('predicted_token: ', predicted_token)
